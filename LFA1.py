2ca832e8e4778f4a791b0bec0fdd095571557b82
hirneagabriel <hirneagabriel@users.noreply.github.com>
2020-03-10 09:24:49 +0200

    LFA1

--- LFA1.py
+++ LFA1.py
@@ -1,33 +1,50 @@
-f=open("dateintrare.txt","r")
-n=int(f.readline())
-graf = [["0"] * n for i in range(n)]
-stareinitiala=int(f.readline())
-starifinale=f.readline().split()
-starifinale=[int(i) for i in starifinale]
-v=f.readline().split()
+f = open("dateintrare.txt","r")
+n = int(f.readline())
+graf = [[" "] * n for i in range(n)]
+noduri = f.readline().split()
+stareinitiala = f.readline()
+
+for i in range(len(noduri)):
+    if stareinitiala[0] == noduri[i]:
+        pozitie = i
+        stare = noduri[i]
+
+starifinale = f.readline().split()
+starifinale = [i for i in starifinale]
+v = f.readline().split()
+
 while v:
-    i=int(v[0])
-    j=int(v[1])
-    if graf[i][j]=="0":
-        graf[i][j]=v[2]
+    for z in range(len(noduri)):
+        if noduri[z] == v[0]:
+            i=z
+        if noduri[z] == v[1]:
+            j = z
+    if graf[i][j] == " ":
+        graf[i][j] = v[2]
     else:
-        graf[i][j]=list(graf[i][j])
+        graf[i][j] = list(graf[i][j])
         graf[i][j].append(v[2])
     v = f.readline().split()
 f.close()
-cuvant=input("cuvant de verificat: ")
+
+print(graf)
+
+cuvant = input("cuvant de verificat: ")
 ok=0
+
 while cuvant:
-    litera=cuvant[0]
-    cuvant=cuvant[:0]+cuvant[1:]
+    litera = cuvant[0]
+    cuvant = cuvant[:0]+cuvant[1:]
     for i in range(n):
-        if litera in graf[stareinitiala][i]:
-            stareinitiala=i
+        if litera in graf[pozitie][i]:
+            stare = noduri[i]
+            pozitie = i
             break
     else:
         print("cuvant neacceptat")
         ok=1
-if stareinitiala in starifinale:
+
+if stare in starifinale and ok == 0:
     print("cuvant acceptat")
 elif ok==0:
     print("cuvant neacceptat")
